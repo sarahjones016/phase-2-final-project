ac961e68b8746a7b43577462cb10886187ec0a69
Patrick Roberts <patrob22@gmail.com>
2023-04-24 15:02:51 -0400

    added functionality to buttons on homepage

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomeSlider from './HomeSlider';
+import { Link } from 'react-router-dom';
 
 function Home({ parks }) {
   return (
@@ -7,8 +8,14 @@ function Home({ parks }) {
         <div className='welcome'>
           <h1>Welcome to NYC Parks!</h1><br></br>
           <p>Your official guide to the true gems of NYC.  Explore the beautiful and historic parks of the City.  Share your favorite spots and connect!  Click below to start exploring!</p><br></br>
-          <button className='home-btn'>Explore the Parks</button>
-          <button className='home-btn'>My Parks</button>
+          
+          <Link to='parks-portal'>
+            <button className='home-btn'>Explore the Parks</button>
+          </Link>
+          <Link to='my-parks'>
+            <button className='home-btn'>My Parks</button>
+          </Link>
+          
         </div>
         
         <HomeSlider parks={parks} />
