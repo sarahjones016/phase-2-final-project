396e26edc5ba5014630669c429ca1365d6b20697
Patrick Roberts <patrob22@gmail.com>
2023-04-24 12:31:11 -0400

    added image and name to slide, added css file

--- src/HomeSlider.js
+++ src/HomeSlider.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Slider.css'
 import { useState } from 'react'
 
 function HomeSlider({ parks }) {
-    //console.log(parks, 'from slider')
+    const [currentSlide, setCurrentSlide] = useState(0);
 
     const slideData = parks.map((park) => {
         return {
@@ -13,8 +14,19 @@ function HomeSlider({ parks }) {
     console.log(slideData)
 
   return (
-    <div>
-        {/* Homepage photo slider */}
+    <div className='slider-container'>
+        {slideData.map((slide, index) => {
+            return (
+                <div className={index === currentSlide ? "current-slide" : "slide"} key={slide.name}>
+                    {index === currentSlide ?
+                    <>
+                        <img src={slide.image} alt={slide.name} />
+                        <h1>{slide.name}</h1>
+                    </> : null}
+                </div>
+            )
+        })
+        }
     </div>
   )
 }
