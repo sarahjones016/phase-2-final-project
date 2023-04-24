e7e8bf3f435b89beccad2717a15d7adad2f12ce8
Patrick Roberts <patrob22@gmail.com>
2023-04-24 11:59:46 -0400

    added h1, p and two buttons to homepage

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,12 @@ import HomeSlider from './HomeSlider';
 function Home({ parks }) {
   return (
     <div>
-        <h1>Homepage</h1>
+        <h1>Welcome to Patrick and Sarah's Website!</h1>
+        <p>This paragraph will contain a little description of what we're doing here!</p>
         {/* Will contain buttons that route to portal and visited pages  */}
         <HomeSlider parks={parks} />
+        <button>Get Started</button>
+        <button>My Parks</button>
     </div>
   )
 }
