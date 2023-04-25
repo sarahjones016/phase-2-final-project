878bca3eca156601c3b5d8569f5efe9064c62930
Patrick Roberts <patrob22@gmail.com>
2023-04-25 10:28:46 -0400

    Added 'visited' to json, setup 'add to my parks' button

--- src/ParkCard.js
+++ src/ParkCard.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
 function ParkCard({ park }) {
-  
+  console.log(park.visited)
+
+  function handleClick() {
+    park.visited = !park.visited;
+    console.log(park.visited)
+
+  }
+
   return (
     <div className='park-card'>
         <img src={park.image} alt={park.name} />
         <h3>{park.name}</h3>
         <p>{park.borough}</p>
+        {park.visited ? <button onClick={handleClick} className='visited'>Remove from My Parks</button>
+        : <button onClick={handleClick} className='not-visited'>Add to My Parks</button>}
     </div>
   )
 }
